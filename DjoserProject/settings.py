d3a08f5e747ccae856025f592405c4a4be99b08e
callistachang <rezinomrah@gmail.com>
2019-06-12 14:25:47 +0800

    Start SPA app and create custom user models

--- DjoserProject/settings.py
+++ DjoserProject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'djoser',
+    'SpaApp',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'SpaApp.SpaUser'
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 http POST http://127.0.0.1:8000/api/users/create/ username="carmeella" password="helloworld1"
+
+http POST http://127.0.0.1:8000/api/users/create/ email="carmeella@example.com" password="helloworld1"
